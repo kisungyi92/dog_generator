0849818fac2a459c61004dc5ae68b2d07ed3b372
Jichan <>
2019-11-08 22:42:29 +0900

    Change to hash route mode

--- src/router/index.js
+++ src/router/index.js
@@ -25,8 +25,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL || 'auto-generator',
+  mode: 'hash',
+  base: process.env.BASE_URL,
   routes
 });
 
